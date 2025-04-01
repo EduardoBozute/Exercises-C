df7009b04bc7b7c9817578365dff58d07cc99b09
Eduardo Bozute <eduardobozutedev@gmail.com>
2025-04-01 16:04:18 -0300

    Create ex14.c

---
+++ ex14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{   
+    int X, A, B, C, D;
+    X = 1;
+    A = 3;
+    B = 5;
+    C = 8;
+    D = 7;
+    if ((X<1) && !(B>D)){
+        printf("V");
+    } else {
+        printf("F");
+    }
+    return 0;
+}
